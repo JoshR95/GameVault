ef152392c14a8e5c6ffc96fc0f65defd0110e5ab
JoshR95 <joshuarickards@hotmail.co.uk>
2026-09-25 15:59:27 +0100

    added ownership policy
    added ownership policy to make sure only users who made this list can update, delete or view

---
+++ app/Policies/GamePolicy.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Game;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class GamePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Game $game): bool
+    {
+        // checks if the id of the owner matches current user id. Can only update if this user owns this row in the database
+        return $user->id === $game->user_id;
+    }
+
+    /**
+     * Determine whether the user can create models/ thier own game library.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Game $game): bool
+    {
+        // checks if the id of the owner matches current user id. Can only update if this user owns this row in the database
+        return $user->id === $game->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Game $game): bool
+    {
+        // checks if the id of the owner matches current user id. Can only delete if this user owns this row in the database
+        return $user->id === $game->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Game $game): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Game $game): bool
+    {
+        return false;
+    }
+}
